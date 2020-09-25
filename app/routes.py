8298c4fbc354be7320d46418dab93e8b7ca79365
Fabio Pereira <fp.jobs122@gmail.com>
2020-09-25 17:48:02 +0100

    Inb4 text

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,16 @@
 from app import app
+from flask import render_template
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return "works!"
+    return render_template('index.html', title = "index")
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html', title = "blog")
+
+
+@app.route("/legal")
+def legal():
+    return render_template('legal.html', title = "legal")
